46c6bd91c65a9e77f6e09f6e37f8cde3cace42d9
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2018-12-25 14:07:18 +0100

    rtl8723bu add framework
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

---
+++ rtl8723bu/dm.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __RTL8723BU_DM_H__
+#define __RTL8723BU_DM_H__
+
+void rtl8723bu_dm_watchdog(struct ieee80211_hw *hw);
+
+#endif
---
+++ rtl8723bu/pwrseq.h
@@ -0,0 +1,5 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __RTL8723BU_PWRSEQ_H__
+#define __RTL8723BU_PWRSEQ_H__
+
+#endif
